e22a7e5577f9cc17a3c0138f16a081f28227cef2
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-02-09 09:46:05 +0100

    Tester

---
+++ ResourceAttribute/Assets/Tester.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Tester : MonoBehaviour {
+
+    [Resource( "" )]
+    public Object[] Objects;
+    
+    [Resource( "Camera Prefab" )]
+    public GameObject Prefab;
+    [Resource( "Logo" )]
+    public Texture2D Texture;
+    [Resource( "SomeTextDoc" )]
+    public TextAsset Text;
+
+    void Start() {
+        this.LoadResources();
+    }
+}
